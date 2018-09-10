e9f9cda06c46988ee17c24f63c199fa8e96ef776
Rafael Vidaurre <narzerus@gmail.com>
2018-09-10 17:36:29 -0300

    test: implement first transaction test
    this serves as an example of boilerplate code for testing interactions
    with the contract without requiring to launch a private blockchain

--- src/app/token/__tests__/actions.js
+++ src/app/token/__tests__/actions.js
@@ -1,12 +1,39 @@
-// import configureMockStore from 'redux-mock-store'
-// import thunk from 'redux-thunk'
-// import * as poly from 'polymathjs'
-// import type { MockStoreEnhanced } from 'redux-mock-store'
-// import * as actions from '../actions'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import BigNumber from 'bignumber.js'
+import type { MockStoreEnhanced } from 'redux-mock-store'
+import * as actions from '../actions'
 
-// jest.mock('polymathjs')
+jest.mock('polymathjs', () => {
+  return {
+    PolyToken: {
+      decimals: 18,
+      symbol: 'POLY',
+      name: 'Polymath Network',
+      allowance: () => 12412  },
+  }
+})
 
-// const mockStore = configureMockStore([thunk])
+const mockedStoreState = {
+  token: {
+    mint: {
+      uploaded: [],
+      uploadedTokens: [],
+    },
+    token: {
+      contract: {
+        mintMulti: jest.fn(),
+        getTransferManager: async () => {
+          return {
+            modifyWhitelistMulti: jest.fn(),
+          }
+        },
+      },
+    },
+  },
+}
+
+const mockStore = configureMockStore([thunk])
 
 /**
  * - Configure PolyToken mock
@@ -14,24 +41,31 @@
  */
 
 describe('Actions: mintTokens', () => {
-  // let store: MockStoreEnhanced<any>
-  // beforeEach(() => {
-  //   store = mockStore()
-  // })
+  let store: MockStoreEnhanced<any>
+  beforeEach(() => {
+    store = mockStore()
+  })
   
-  test('requires two transactions by default', () => {
-    /**
-     * - Dispatch transaction
-     * - Check that dispatch was async-dispatched with 2 arrays
-     */
-    // console.log('PolyToken', PolyToken)
-    // // const res = PolyToken.allowance()
-    // // console.log('res', res)
-    // store = mockStore()
-    // store.dispatch(actions.mintTokens())
+  test('requires two transactions by default', async () => {
+    const storeState = { ...mockedStoreState }
+    const investorAddresses = [{ address: 'address1' }, { address: 'address2' }]
+    const investorTokens = [BigNumber(500), BigNumber(100)]
+    const expectedAddresses = investorAddresses.map((investor) => investor.address)
+    storeState.token.token.contract.mintMulti = jest.fn()
+    storeState.token.mint.uploaded = investorAddresses
+    storeState.token.mint.uploadedTokens = investorTokens
+    store = mockStore(storeState)
+    await store.dispatch(actions.mintTokens())
+    const dispatchedAction = store.getActions()[0]
+    expect(dispatchedAction.titles).toEqual(['Whitelisting Addresses', 'Minting Tokens'])
+    expect(storeState.token.token.contract.mintMulti).toHaveBeenCalledWith(expectedAddresses, investorTokens)
   })
 
   test('skips first transaction if previously executed', () => {
+    
+  })
+
+  test.skip('dispatches tx/START action on start ', () => {
     
   })
   
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,46 @@
 // @flow
 
+// import Web3 from 'web3'
+// import Contract from 'polymathjs'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+// let snapshotId
+// const web3 = new Web3('ws://localhost:8545')
+// jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000
+
 Enzyme.configure({ adapter: new Adapter() })
+
+// beforeAll(async (done) => {
+//   const id = await web3.eth.net.getId()
+//   const [account] = await web3.eth.getAccounts()
+//   Contract.setParams({
+//     id, account, web3, web3WS: web3, txHashCallback: () => {}, txEndCallback: () => {},
+//   })
+
+//   await web3.currentProvider.send({
+//     jsonrpc: '2.0',
+//     method: 'evm_snapshot',
+//     id: new Date().getTime(),
+//   }, (err, result) => {
+//     if (err || !result) {
+//       done.fail()
+//       return
+//     }
+
+//     snapshotId = web3.utils.toDecimal(result.result)
+//   })
+// })
+
+// afterAll((done) => {
+//   web3.currentProvider.send({
+//     jsonrpc: '2.0',
+//     method: 'evm_revert',
+//     id: new Date().getTime(),
+//     params: [snapshotId],
+//   }, (err) => {
+//     if (err) {
+//       done.fail()
+//     }
+//   })
+// })
